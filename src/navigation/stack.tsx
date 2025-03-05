9f0b891e8a5fd5664f184f1f80d532e707a20ceb
Shaan Ayub <112044382+Shaan3274@users.noreply.github.com>
2025-03-05 22:09:16 +0500

    Updated

---
+++ src/navigation/stack.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const stack = () => {
+  return (
+    <View>
+      <Text>stack</Text>
+    </View>
+  )
+}
+
+export default stack
+
+const styles = StyleSheet.create({})
---
+++ src/themes/colors.js
@@ -0,0 +1,8 @@
+const colors={
+    primary:"#0052CC",
+    secondary:"#FF8C00",
+    background:"#F4F5F7",
+    headingsText:"#2C3E50",
+    normalText:"#7F8C8D",
+    whiteText:"#FFFFFF"
+}
